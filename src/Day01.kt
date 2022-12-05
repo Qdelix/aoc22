2d3f1959b9c25004320d4c38d9466cd2e5a786f4
Qdelix <dawidkudela.1997@gmail.com>
2022-12-05 18:38:32 +0100

    Second part of day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -17,7 +17,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var firstSum = 0
+        var secondSum = 0
+        var thirdSum = 0
+        var actualSum = 0
+
+        input.forEach { inputLine ->
+            if (inputLine.isNotEmpty()) {
+                actualSum += inputLine.toInt()
+            } else {
+                if (actualSum > firstSum) {
+                    thirdSum = secondSum
+                    secondSum = firstSum
+                    firstSum = actualSum
+                } else if (actualSum > secondSum) {
+                    thirdSum = secondSum
+                    secondSum = actualSum
+                } else if (actualSum > thirdSum) {
+                    thirdSum = actualSum
+                }
+                actualSum = 0
+            }
+        }
+        return firstSum + secondSum + thirdSum
     }
 
     val input = readInput("Day01")
