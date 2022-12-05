7f6d74e44c205f9bdbc991151f5e69bbda3cd41e
Qdelix <dawidkudela.1997@gmail.com>
2022-12-05 18:26:38 +0100

    First part of day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var highestSum = 0
+        var actualSum = 0
+
+        input.forEach { inputLine ->
+            if (inputLine.isNotEmpty()) {
+                actualSum += inputLine.toInt()
+            } else {
+                if (actualSum > highestSum) {
+                    highestSum = actualSum
+                }
+                actualSum = 0
+            }
+        }
+        return highestSum
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
